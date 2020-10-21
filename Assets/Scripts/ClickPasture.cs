d4ee1fb1f58cdbda72282e63ff1717528a8751f8
laurynbranham47 <lbranham@wisc.edu>
2020-10-20 20:30:30 -0500

    added menu button

---
+++ Assets/Scripts/ClickPasture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickPasture : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera PastureSideCamera;
+
+    void onMouseDown(){
+        Destroy(gameObject);
+    }
+
+    //create UI object for back button to go back to home screen
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int totalMoola;
+    //private static int minMoola;
+    public Text PlayerMoola;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalMoola = 200;
+        //minMoola = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoola.text = "" + totalMoola;
+    }
+
+    // void updateMoola(){
+        
+    // }
+}
