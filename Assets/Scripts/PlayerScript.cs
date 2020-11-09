893b9325f7f0807df5217bf372570904ba94a944
laurynbranham47 <lbranham@wisc.edu>
2020-11-09 16:15:29 -0600

    Added cow instantiation process to player class

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,14 +5,20 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
+    // STATIC Vars
     static public int totalMoola = 200;
-    //private static int minMoola;
+    static public List<GameObject> CowReferences;
+
+    //Vars
     public Text PlayerMoola;
+    public GameObject CowPreFab;
 
     // Start is called before the first frame update
     void Start()
     {
         PlayerMoola.text = "" + totalMoola;
+        CowReferences = new List<GameObject>();
+        CowReferences.Add((GameObject)Instantiate(CowPreFab, new Vector3(18.72f, 1.24f, 8.62f), Quaternion.identity));
     }
 
     // Update is called once per frame
@@ -31,5 +37,14 @@ public class PlayerScript : MonoBehaviour
         } else {
             totalMoola -= moola;
         }
+    }
+
+    public void AddCow(GameObject cow){ // TODO - TEST
+        Vector3 RandomSpawn = new Vector3(Random.Range(18f, 20f), 1.24f, Random.Range(7.5f, 9.5f));
+        CowReferences.Add((GameObject)Instantiate(CowPreFab, RandomSpawn, Quaternion.identity));
+    }
+
+    public void SubtractCow(GameObject cow){
+        //TODO
     }
 }
