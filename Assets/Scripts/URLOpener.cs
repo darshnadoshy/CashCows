0e2618c33f31c086ae1e1e5778ab32438e91deb9
snehapolishetty <spolishetty@wisc.edu>
2020-11-24 13:48:21 -0600

    codex with links

---
+++ Assets/Scripts/URLOpener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLOpener : MonoBehaviour
+{
+    public string Url1; 
+    public string Url2; 
+
+    public void Open(int val)
+    {
+    	if (val == 1) { 
+    		Application.OpenURL(Url1); 
+    	}
+    	if (val == 2) { 
+    		Application.OpenURL(Url2); 
+    	}
+    }
+}
