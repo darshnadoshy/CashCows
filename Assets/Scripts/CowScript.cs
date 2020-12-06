3baa5e222da5166e36d38cd6e9bf8cd679ad33dd
laurynbranham47 <lbranham@wisc.edu>
2020-12-05 21:53:56 -0600

    Added buttons in for feed, clean, milk cow

--- Assets/Scripts/CowScript.cs
+++ Assets/Scripts/CowScript.cs
@@ -7,43 +7,52 @@ public class CowScript : MonoBehaviour
 {
     public int maxMilk = 5;
     public int currentMilk;
+    int milkingFlag;
     public GameObject popUp;
-    public MilkCowScript milkBar;
+    public MilkCowScript milkScript;
     public GameObject time;
     public GameObject player;
-    //public StaticCoroutineRunner COR;
+    public GameObject milkContainer;
+    public GameObject buttonContainer;
+    public GameObject BackButton;
 
     // Start is called before the first frame update
     void Start()
     {
         currentMilk = 0;
-        milkBar.SetMaxMilk(maxMilk);
+        milkScript.SetMaxMilk(maxMilk);
         popUp.SetActive(false);
+        milkContainer.SetActive(false);
+        buttonContainer.SetActive(true);
+        BackButton.SetActive(false);
+        milkingFlag = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            AddMilk();
-            milkBar.SetMilk(currentMilk);
-            if(currentMilk == maxMilk){
-                player.GetComponent<PlayerScript>().AddMoola(100);
-                popUp.SetActive(true);
-                List<CowObject> list = player.GetComponent<PlayerScript>().GetListCows();
+        if(milkingFlag == 1){
+            if (Input.GetMouseButtonDown(0)) {
+                AddMilk();
+                milkScript.SetMilk(currentMilk);
+                if(currentMilk == maxMilk){
+                    player.GetComponent<PlayerScript>().AddMoola(100);
+                    popUp.SetActive(true);
+                    List<CowObject> list = player.GetComponent<PlayerScript>().GetListCows();
 
-                // Find currently milked cow and set milked status to true, also start reset coroutine
-                Debug.Log("MILKED: " + PlayerPrefs.GetString("currentlyBeingMilked"));
-                for(int i = 0; i < list.Count; i++){
-                    if(list[i].getName() == PlayerPrefs.GetString("currentlyBeingMilked")){
-                        list[i].SetMilkedStatus(true);
-                        PlayerPrefs.SetString("currentlyBeingMilked", "None");
-                        StaticCoroutineRunner.RunCoroutine(ResetMilk(list[i]));
-                        break;
+                    // Find currently milked cow and set milked status to true, also start reset coroutine
+                    Debug.Log("MILKED: " + PlayerPrefs.GetString("currentlyBeingMilked"));
+                    for(int i = 0; i < list.Count; i++){
+                        if(list[i].getName() == PlayerPrefs.GetString("currentlyBeingMilked")){
+                            list[i].SetMilkedStatus(true);
+                            PlayerPrefs.SetString("currentlyBeingMilked", "None");
+                            StaticCoroutineRunner.RunCoroutine(ResetMilk(list[i]));
+                            break;
+                        }
                     }
-                }
-                return;
-            } 
+                    return;
+                } 
+            }
         }
     }
 
@@ -52,11 +61,36 @@ public class CowScript : MonoBehaviour
     }
 
     // Resets cow's milked status and lets button be interactable again
-    // TODO NEEDS TO GET SWITCHED TO ANOTHER SCRIPT ///////////
     IEnumerator ResetMilk(CowObject cowObj){
         yield return new WaitForSeconds(20);
         cowObj.SetMilkedStatus(false);
         Debug.Log("Hit 20 seconds. Milked status: " + cowObj.GetMilkedStatus());
     }
 
+    public void Clicked(){
+        buttonContainer.SetActive(false);
+    }
+
+    public void ClickMilkButton(){
+        milkingFlag = 1;
+        milkContainer.SetActive(true);
+        BackButton.SetActive(true);
+    }
+
+    public void ClickCleanButton(){
+        
+    }
+
+    public void ClickFeedButton(){
+
+    }
+
+    public void ClickBackButton(){
+        buttonContainer.SetActive(true);
+        milkContainer.SetActive(false);
+        milkingFlag = 0;
+        BackButton.SetActive(false);
+        popUp.SetActive(false);
+        //and feed & clean containers = false
+    }
 }
