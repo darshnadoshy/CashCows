5e1ce84937775ac67d228bce83353b244ebc1be0
laurynbranham47 <lbranham@wisc.edu>
2020-12-10 14:01:23 -0600

    added statement

--- Assets/Scripts/StatementsScript.cs
+++ Assets/Scripts/StatementsScript.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 
 public class StatementsScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public Text message;
 
-    // Update is called once per frame
-    void Update()
+    public void HandleInputData(int value)
     {
-        
+        if (value == 1)
+        {
+            message.text = "Date\t\t\tDescription\t\tAmount\n\n\n" + "Dec 10\t\tTransfer\t\t\t+50" + 
+                            "\n\n\n\t\t\t\tTotal = 50";
+        }
+
+        if (value == 2)
+        {
+            message.text = "Date\t\t\tDescription\t\tAmount\n\n\n" + "Dec 10\t\tTransfer\t\t\t-50\n\n" +
+                            "Dec 10\t\tStore\t\t\t\t+90\n\n" + "Dec 10\t\tStore\t\t\t\t-190" + 
+                            "\n\n\t\t\t\tTotal = 150";
+        }
     }
 }
